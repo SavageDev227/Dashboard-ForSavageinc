54c9922d77454c8be4ee0866b899e2fe6aef6c71
Savagegamez227 <79803014+Savagegamez227@users.noreply.github.com>
2021-03-01 06:22:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -48,3 +48,22 @@ function logout(){
 function newform(){
  window.open("Admin.html") 
 } 
+
+function signUp(){
+  
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+
+
+}
